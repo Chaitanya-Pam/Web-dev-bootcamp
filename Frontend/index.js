981900a1d4c4412a9e5fdd874b06666e264c6cd0
Chaitanya-Pam <chaitanyar934@gmail.com>
2026-08-13 16:32:31 +0530

    Done Switching the Users

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,23 @@
 //alert("Welcome to Chaitanya's Page");
+var users=[
+    {
+        "name":"Tom Holland",
+        "gender":"Male",
+        "image":"john.png"
+    },
+    {
+        "name":"Zendaya",
+        "gender":"Female",
+        "image":"jane.png"
+    }
+]
+var curId=0;
+function switchUser(){
+    curId=(curId+1)%2;
+    var userName=document.getElementById("user-name");
+    var userGender=document.getElementById("user-gender");
+    var userImage=document.getElementById("user-image");
+    userName.innerHTML=users[curId].name;
+    userGender.innerHTML=users[curId].gender;
+    userImage.src=users[curId].image;   
+}
